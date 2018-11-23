cf43b6d6552006aeda2b575aed2de8af3a156b9f
Violeta Georgieva <vgeorgieva@pivotal.io>
2018-11-23 14:12:55 +0200

    Integration with Reactor Logback

--- twitter-reactor-netty-ui/src/main/java/com/example/demo/ReactorNettyApplication.java
+++ twitter-reactor-netty-ui/src/main/java/com/example/demo/ReactorNettyApplication.java
@@ -8,11 +8,19 @@ import com.example.demo.twitter.TwitterStreamService;
 import com.example.demo.utils.SseHandler;
 import com.example.demo.utils.StaticResourceHandler;
 import com.example.demo.utils.WebSocketHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+import reactor.function.TupleUtils;
 import reactor.netty.DisposableServer;
 import reactor.netty.http.server.HttpServer;
+import reactor.util.function.Tuples;
+
+import java.util.concurrent.TimeUnit;
 
 public class ReactorNettyApplication {
+    public static final Logger LOG = LoggerFactory.getLogger(ReactorNettyApplication.class);
 
     public static void main(String[] args) {
         TwitterStreamService twitterStreamService = new KafkaTwitterStreamService();
@@ -20,6 +28,35 @@ public class ReactorNettyApplication {
         Flux<Tweet> tweetsFlux =
                 twitterStreamService.stream()
                         .transform(locationEnrichService::enrich)
+                        .map(d -> Tuples.of(System.nanoTime(), d))
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .log()
+                        .doOnNext(TupleUtils.consumer((timestamp, tweet) -> {
+                            LOG.error("Execution Time {}", System.nanoTime() - timestamp);
+                            tweet.toString();
+                        }))
+                        .map(TupleUtils.function((t, tweet) -> tweet))
                         .publish()
                         .autoConnect(1);
 
